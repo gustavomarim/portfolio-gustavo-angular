2f9190bde646d133557deeeb5bb87f8d5ccc4dc2
Gustavo Dantas <66189039+gustavomarim@users.noreply.github.com>
2023-02-24 16:41:47 -0300

    feat(components): adiciona componente introduction (#2)
    Este commit adiciona a sessão responsável por fazer a introdução do perfil do
    portfólio.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HeaderComponent, IntroductionComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
