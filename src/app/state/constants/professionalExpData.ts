2d9f5e4147b2d09e9bb374dc404dc0d36d88844b
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-24 19:11:39 -0300

    feat(state): adiciona constante com informações profissionais Este commit adiciona a constante responsável por armazenar as informações profissionais do portfólio. Futuramente pode ser substituída por uma API que devolva as informações necessárias.

---
+++ src/app/state/constants/professionalExpData.ts
@@ -0,0 +1,26 @@
+import { ProfessionalExperience } from "src/app/core/models/ProfessionalExperience";
+
+export const professionalExp: ProfessionalExperience[] = [
+  {
+    companyName: 'Universidade Paulista - UNIP',
+    workingTime: '2018 - 2022',
+    positionInCompany: 'Arquivista Jr',
+    descriptionOfActivities:
+      'Responsável pela organização e manutenção dos arquivos dos colaboradores, recebimento de documentações, acompanhamento de e-mails, realização de planilhas, para controle do setor, serviços externos administrativos e apoio a outras áreas como: almoxarife, expedição.',
+    companySkills: [
+      'Microsoft Outlook',
+      'Atendimento ao cliente',
+      'Trabalho em equipe',
+      'Microsoft Excel',
+      'Microsoft Office',
+    ],
+  },
+  {
+    companyName: 'SBK BPO',
+    workingTime: '2014 - 2017',
+    positionInCompany: 'Auxiliar Adm. Operacional',
+    descriptionOfActivities:
+      'Responsável pelas rotinas administrativas em analises de processos, leitura e interpretação, cadastros de informações pessoais ou de processos, realizando pesquisas em sistemas internos e externos da empresa.',
+    companySkills: ['Microsoft Excel', 'Microsoft Office'],
+  },
+];
