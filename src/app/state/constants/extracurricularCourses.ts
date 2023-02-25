65b2e205324431bc6504017f973b1e261c84165e
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-25 13:38:41 -0300

    feat(components): adiciona sessão de experiência acadêmica Este commit adiciona o componente responsável por exibir as informações acadêmicas do portfólio.

---
+++ src/app/state/constants/extracurricularCourses.ts
@@ -0,0 +1,32 @@
+import { ExtracurricularCourse } from 'src/app/core/models/ExtracurricularCourse';
+
+export const extracurricularCourses: ExtracurricularCourse[] = [
+  {
+    name: 'TypeScript',
+    hours: 22,
+  },
+  {
+    name: 'React Completo',
+    hours: 36,
+  },
+  {
+    name: 'Angular 9 - Essencial',
+    hours: 13,
+  },
+  {
+    name: 'JavaScript Completo ES6+',
+    hours: 74,
+  },
+  {
+    name: 'Treinamento em Desenvolvimento Fullstack',
+    hours: 120,
+  },
+  {
+    name: 'C++ Essencial',
+    hours: 22,
+  },
+  {
+    name: 'Java Completo',
+    hours: 76,
+  },
+];
